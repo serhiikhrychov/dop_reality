3a0dc6f656cecd6144727619350d47c98815bfdc
Serhii Khrychov <serhii.khrychov@PMCORP.LOC>
2021-11-10 17:32:44 +0200

    device orientation added

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ animate();
 
 function createFigyre() {
   // const geometry = new THREE.ParametricGeometry(astroidalHelicoidEquantion, 10,30);
-  const geometry = new ParametricGeometry(hyperbolicHelicoidEquantion, 100,100);
+  const geometry = new ParametricGeometry(hyperbolicHelicoidEquantion, 50,50);
   const material = new THREE.MeshNormalMaterial();
   return new THREE.Mesh( geometry, material );
 }
@@ -47,3 +47,50 @@ function hyperbolicHelicoidEquantion(u, v, target) {
 
   return  target.set(x,y,z);
 }
+
+function getRotationMatrix(alpha, beta, gamma) {
+  var degtorad = Math.PI / 180; // Degree-to-Radian conversion
+  var _x = beta ? beta * degtorad : 0; // beta value
+  var _y = gamma ? gamma * degtorad : 0; // gamma value
+  var _z = alpha ? alpha * degtorad : 0; // alpha value
+  
+  var cX = Math.cos(_x);
+  var cY = Math.cos(_y);
+  var cZ = Math.cos(_z);
+  var sX = Math.sin(_x);
+  var sY = Math.sin(_y);
+  var sZ = Math.sin(_z);
+  
+  //
+  // ZXY rotation matrix construction.
+  
+  var m11 = cZ * cY - sZ * sX * sY;
+  var m12 = -cX * sZ;
+  var m13 = cY * sZ * sX + cZ * sY;
+  
+  var m21 = cY * sZ + cZ * sX * sY;
+  var m22 = cZ * cX;
+  var m23 = sZ * sY - cZ * cY * sX;
+  
+  var m31 = -cX * sY;
+  var m32 = sX;
+  var m33 = cX * cY;
+  
+  return [m11, m12, m13, m21, m22, m23, m31, m32, m33];
+  }
+  
+  var threejs_matrix4 = new THREE.Matrix4();
+  
+  window.addEventListener('deviceorientation', e => {
+  var m2 = getRotationMatrix(e.alpha, e.beta, e.gamma);
+  
+  threejs_matrix4.set(
+    m2[0], m2[1], m2[2], 0,
+    m2[3], m2[4], m2[5], 0,
+    m2[6], m2[7], m2[8], 0,
+    0, 0, 0, 1
+  );
+  hyperbolicHelicoid.rotation.setFromRotationMatrix(threejs_matrix4);
+  
+  renderer.render(scene, camera);
+  });
